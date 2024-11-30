14edb4d1e2c5c9ff18547793b37804b41cd6d6e6
NLDaza <nelson.licot@gmail.com>
2024-11-30 16:47:26 +0100

    Boton de borrado de la base completo, falta reiniciar la app

--- app/src/main/java/com/taller/myapplication/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/taller/myapplication/ui/screens/SettingsScreen.kt
@@ -1,6 +1,8 @@
 package com.taller.myapplication.ui.screens
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -32,6 +34,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -71,6 +74,7 @@ fun SettingsScreen(navController: NavHostController) {
 }
 @Composable
 fun ContentSettingsScreen(it:PaddingValues){
+    val context = LocalContext.current //Obtiene el contexto actual para el borrado de la bbdd
     var showDialog by remember { mutableStateOf(false) }
     Column(
         modifier = Modifier
@@ -82,14 +86,17 @@ fun ContentSettingsScreen(it:PaddingValues){
     ){
         SwitchBtnDayNight()
         Button(onClick = {showDialog = true}) {
-            Text("Eliminar todas las entradas")
+            Text("Eliminar base de datos")
         }
         if (showDialog){
             AlertDialog(
                 onDismissRequest = {showDialog= false},
                 confirmButton = {
                     TextButton(
-                        onClick = {}
+                        onClick = {
+                            deleteDatabase(context)
+                            showDialog = false
+                        }
                     ) {
                         Text("Eliminar")
                     }
@@ -106,7 +113,15 @@ fun ContentSettingsScreen(it:PaddingValues){
             )
         }
     }
-
+}
+fun deleteDatabase(context: Context) {
+    val databaseName = "db_diary"
+    val isDeleted = context.deleteDatabase(databaseName)
+    if (isDeleted) {
+        Toast.makeText(context, "Base de datos eliminada con éxito", Toast.LENGTH_SHORT).show()
+    } else {
+        Toast.makeText(context, "No se pudo eliminar la base de datos", Toast.LENGTH_SHORT).show()
+    }
 }
 
 
